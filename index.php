abae6cf475a120690c928180231edf6f556b6fa7
Casey Milne <casey.milne@eatbuildplay.com>
2020-11-29 13:33:04 -0500

    init commit with table structure

---
+++ index.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <title>Exercise Tracker</title>
+</head>
+
+<body>
+
+  <!-- Header -->
+  <header>
+    <img src="https://picsum.photos/50" />
+  </header>
+
+  <div id="app-body">
+
+    <table>
+
+      <thead>
+        <tr>
+          <th>Date</th>
+          <th>Exercise</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        <tr>
+          <td>2020-11-29</td>
+          <td>Bodyweight Squats</td>
+        </tr>
+      </tbody>
+
+    </table>
+
+  </div>
+
+  <!-- Foter -->
+  <footer>
+    <img src="https://picsum.photos/50" />
+  </footer>
+
+</body>
+
+</html>
+
+<style>
+
+body {
+  margin: 0;
+}
+
+header {
+  background: #A8A8A8;
+  margin-bottom: 50px;
+}
+
+footer {
+  text-align: right;
+  background: #B8B8B8;
+  margin-top: 50px;
+}
+
+</style>
